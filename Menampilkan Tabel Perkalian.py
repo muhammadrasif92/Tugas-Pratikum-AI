e75d77c1ac768202c92e18f40ac936ad9abc9953
muhammadrasif92 <121092802+muhammadrasif92@users.noreply.github.com>
2023-03-02 09:21:10 +0700

    Add files via upload

---
+++ Menampilkan Tabel Perkalian.py
@@ -0,0 +1,8 @@
+# Menginput Angka
+angka = int(input("Menampilkan Tabel Perkalian dari: "))
+ 
+# Menghitung 10 Kali dari Kisaran 1 sampai 10
+for i in range(1, 11): #range untuk menghitung perkalian sesuai dengan kisaran yang ditentukan dan  for untuk perulangan
+ 
+#Menampilkan Tabel Perkalian
+    print(angka, 'x', i, '=', angka*i) 
---
+++ Menghitung volume.py
@@ -0,0 +1,10 @@
+print ("PROGRAM PYTHON MENGHITUNG VOLUME BALOK")
+p = float(input("Panjang = "))
+l = float(input("Lebar   = "))
+t = float(input("Tinggi  = "))
+
+v = p*l*t
+
+print ("Volume Balok = %0.2f" %v)
+
+#integer (bilangan bulat) atau float (bilangan riil).
